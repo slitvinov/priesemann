06e179c80c797df8078926d8870c027f7904e59b
Sergey Litvinov <slitvinov@gmail.com>
2020-05-29 10:49:25 +0200

    unused functions in model_helper

--- covid19_inference/model_helper.py
+++ covid19_inference/model_helper.py
@@ -2,12 +2,6 @@ import theano
 import theano.tensor as tt
 import numpy as np
 
-
-def tt_lognormal(x, mu, sigma):
-    distr = 1/x* tt.exp(-((tt.log(x) - mu) ** 2) / (2 * sigma ** 2))
-    return distr / tt.sum(distr, axis=0)
-
-
 def delay_cases(new_I_t, len_new_I_t, len_out, delay, delay_diff):
     """
         Delays (time shifts) the input new_I_t by delay.
@@ -63,32 +57,6 @@ def make_delay_matrix(n_rows, n_columns, initial_delay=0):
         diagonal = np.ones(size - i) * (initial_delay - i)
         mat += np.diag(diagonal, -i)
     return mat[:n_rows, :n_columns]
-
-
-def apply_delay(array, delay, sigma_delay, delay_mat):
-    mat = tt_lognormal(delay_mat, mu=np.log(delay), sigma=sigma_delay)
-    return tt.dot(array, mat)
-
-
-def delay_cases_lognormal(
-    input_arr,
-    len_input_arr,
-    len_output_arr,
-    median_delay,
-    scale_delay,
-    delay_betw_input_output,
-):
-    delay_mat = make_delay_matrix(
-        n_rows=len_input_arr,
-        n_columns=len_output_arr,
-        initial_delay=delay_betw_input_output,
-    )
-    delay_mat[
-        delay_mat < 0.01
-    ] = 0.01  # needed because negative values lead to nans in the lognormal distribution.
-    delayed_arr = apply_delay(input_arr, median_delay, scale_delay, delay_mat)
-    return delayed_arr
-
 
 def interpolate(array, delay, delay_matrix):
     """
